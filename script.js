2bf59a5430552c36d7b6a5e78796d190eada5fef
sparklingashes <90340188+sparklingashes@users.noreply.github.com>
2022-01-27 19:29:56 +0530

    computation of simple interest function

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    t = document.getELementById("years").value;
+    a = p+((p*r*t)/100);
+    y = new Date().getFullYear();
+    document.write("If you deposit ",p);
+    document.write("at an interest rate of ",r,"%");
+    document.write("You will receive an amount of ",a);
+    document.write("in the year",t+y);
     
 }
         

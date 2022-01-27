c020c638ff2ffe54adbb2e491d031dd100e40b5e
sparklingashes <90340188+sparklingashes@users.noreply.github.com>
2022-01-27 22:56:04 +0530

    javascript updated

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    t = document.getELementById("years").value;
-    a = p+((p*r*t)/100);
-    y = new Date().getFullYear();
-    document.write("If you deposit ",p);
-    document.write("at an interest rate of ",r,"%");
-    document.write("You will receive an amount of ",a);
-    document.write("in the year",t+y);
+    var principal = document.getElementById("principal").value;
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
     
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>,"  + "<br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%," + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>," + "<br> in the year " + "<span class='highlight'>" + yearNow + "</span>.";
+
 }
+function updateRate()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
+}
+        
         
